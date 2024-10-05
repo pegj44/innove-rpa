e7eb580d6cbd28f6bb391f79130ba4a5d973846f
pegj44 <31127167+pegj44@users.noreply.github.com>
2024-10-05 22:33:16 +0800

    update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,17 @@
 <x-app-layout>
     <div class="bg-white dark:bg-gray-800 mt-14 overflow-hidden shadow-sm sm:rounded-lg">
         <div class="p-6 text-gray-900 dark:text-gray-100">
-            {{ __("Hello World! - (Report overviews will be displayed here)") }}
+
+            <div class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                <h2 class="font-bold text-4xl text-green-500">+$1,250.00</h2>
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Daily Profit</h5>
+            </div>
+
+            <div class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                <h2 class="font-bold text-4xl text-green-500">+$12,000.00</h2>
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Total Profit</h5>
+
+            </div>
         </div>
     </div>
 </x-app-layout>
