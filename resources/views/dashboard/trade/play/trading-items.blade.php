7cc35f97377ac22dc4306ff97b265df05500997b
paul-caveim <paul@caveim.com>
2025-01-29 05:33:59 +0800

    update

--- resources/views/dashboard/trade/play/trading-items.blade.php
+++ resources/views/dashboard/trade/play/trading-items.blade.php
@@ -65,6 +65,7 @@
                     <form method="post" action="{{ route('trade.remove-pair', $tradedItem['queue_db_id']) }}" style="height: 24px;">
                         @csrf
                         @method('DELETE')
+                        <input type="hidden" name="type" value="close">
                         <button type="submit" class="">
                             <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18 17.94 6M18 18 6.06 6"/>
