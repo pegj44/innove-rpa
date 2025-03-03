c58cd05b361be9bf4ae843ff1b5e294ef32a668f
paul-caveim <paul@caveim.com>
2025-03-04 07:37:44 +0800

    update

--- resources/views/dashboard/trade/play/paired-item.blade.php
+++ resources/views/dashboard/trade/play/paired-item.blade.php
@@ -90,21 +90,21 @@
     </button>
 {{--    <div class="border border-gray-700 border-l-0 flex items-center p-3 remove-pair">--}}
     <div class="border border-gray-700 border-l-0 flex items-center p-3 remove-pair {{ (!$canCloseTrade)? 'hidden' : '' }}">
-        <a href="#" class="" onclick="cancelPairing(event, {{ $pairedItemData['queue_db_id'] }})">
-            <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18 17.94 6M18 18 6.06 6"/>
-            </svg>
-        </a>
-{{--        <form method="post" data-pair_id="{{ $pairedItemData['queue_db_id'] }}" action="{{ route('trade.remove-pair', $pairedItemData['queue_db_id']) }}" style="height: 24px;">--}}
-{{--            @csrf--}}
-{{--            @method('DELETE')--}}
-{{--            <input type="hidden" name="type" value="cancel">--}}
-{{--            <button type="submit" class="">--}}
-{{--                <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">--}}
-{{--                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18 17.94 6M18 18 6.06 6"/>--}}
-{{--                </svg>--}}
-{{--            </button>--}}
-{{--        </form>--}}
+{{--        <a href="#" class="" onclick="cancelPairing(event, {{ $pairedItemData['queue_db_id'] }})">--}}
+{{--            <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">--}}
+{{--                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18 17.94 6M18 18 6.06 6"/>--}}
+{{--            </svg>--}}
+{{--        </a>--}}
+        <form method="post" data-pair_id="{{ $pairedItemData['queue_db_id'] }}" action="{{ route('trade.remove-pair', $pairedItemData['queue_db_id']) }}" style="height: 24px;">
+            @csrf
+            @method('DELETE')
+            <input type="hidden" name="type" value="cancel">
+            <button type="submit" class="">
+                <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18 17.94 6M18 18 6.06 6"/>
+                </svg>
+            </button>
+        </form>
     </div>
 </h2>
 
